31363cc63b38af7f244db36051bf548073207a7a
Your Name <14658@utcv.edu.mx>
2019-08-19 12:55:55 -0500

    first commit

---
+++ lib/busi_api_web/router.ex
@@ -0,0 +1,26 @@
+defmodule BusiApiWeb.Router do
+  use BusiApiWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", BusiApiWeb do
+    pipe_through :api
+  end
+
+  pipeline :browser do
+    plug(:accepts, ["html"])
+  end
+
+  scope "/", BusiApiWeb do
+    pipe_through :browser
+    get "/", DefaultController, :index
+  end
+
+  scope "/api", BusiApiWeb do
+  pipe_through :api
+  resources "/businesses", BusinessController, except: [:new, :edit]
+  end
+
+end
